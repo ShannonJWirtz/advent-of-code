f2474f8bed34aa2dabb1c7b3e0a9a32d204afe2b
Shannon Wirtz <shannon@mybuilder.com>
2021-12-08 18:22:08 +0000

    slight tidying

--- 2021/day_8/solution.py
+++ 2021/day_8/solution.py
@@ -29,24 +29,23 @@ def get_decoded_output(line):
 
     missing_segment_codes = {seg: [code for code in unique_codes if seg not in code] for seg in 'abcdefg'}
 
-    number_encodings[2] = [codes[0] for codes in missing_segment_codes.values() if len(codes) == 1][0]
+    number_encodings[2] = next(codes[0] for codes in missing_segment_codes.values() if len(codes) == 1)
 
-    five_six = [codes for codes in missing_segment_codes.values() if len(codes) == 2 and number_encodings[1] not in codes][0]
+    five_six = next(codes for codes in missing_segment_codes.values() if len(codes) == 2 and number_encodings[1] not in codes)
 
-    number_encodings[5] = [code for code in five_six if len(code) == 5][0]
+    number_encodings[5] = next(code for code in five_six if len(code) == 5)
 
-    number_encodings[6] = [code for code in five_six if len(code) == 6][0]
+    number_encodings[6] = next(code for code in five_six if len(code) == 6)
 
-    number_encodings[0] = [ codes[0]
+    number_encodings[0] = next(codes[0]
                            for codes in
                            [[code for code in codes if code not in number_encodings.values()]
                             for seg, codes in segment_codes.items()]
-                           if len(codes) == 1
-                           ][0]
+                           if len(codes) == 1)
 
-    number_encodings[3] = [code for code in unique_codes if code not in number_encodings.values() and len(code) == 5][0]
+    number_encodings[3] = next(code for code in unique_codes if code not in number_encodings.values() and len(code) == 5)
 
-    number_encodings[9] = [code for code in unique_codes if code not in number_encodings.values() and len(code) == 6][0]
+    number_encodings[9] = next(code for code in unique_codes if code not in number_encodings.values() and len(code) == 6)
 
     number_decodings = {frozenset(code): number for number, code in number_encodings.items()}
 
