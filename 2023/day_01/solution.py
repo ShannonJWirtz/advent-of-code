a3508bdf28c0ccfd74b4cba1cb04df65032784d2
Shannon Wirtz <shannon@mybuilder.com>
2023-12-03 16:40:21 +0000

    2023 day 01 solution

--- 2023/day_01/solution.py
+++ 2023/day_01/solution.py
@@ -4,7 +4,7 @@ number_words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 
 number_words_regex = '|'.join(number_words)
 
 def get_calibration_value(line):
-    digits = re.findall('\d', line)
+    digits = re.findall(r'\d', line)
     return int(digits[0]+ digits[len(digits)-1])
 
 def get_sum(input):
@@ -21,9 +21,19 @@ def get_integer_from_string(s):
     
 
 def get_calibration_value_part_2(line):
-    number_words_regex = '1|2|3|4|5|6|7|8|9|0|one|two|three|four|five|six|seven|eight|nine'
-    digits = re.findall('\d|'+number_words_regex, line)
-    return int(get_integer_from_string(digits[0])+ get_integer_from_string(digits[len(digits)-1]))
+    first_index, first_number = len(line), ''
+    for i in list('123456789') + number_words:
+        if i in line and line.index(i) < first_index:
+            first_index = line.index(i)
+            first_number = i
+
+    last_index, last_number = len(line), ''
+    for i in list('123456789') + number_words:
+        if i in line and line[::-1].index(i[::-1]) < last_index:
+            last_index = line[::-1].index(i[::-1])
+            last_number = i
+
+    return int(get_integer_from_string(first_number)+ get_integer_from_string(last_number))
 
 def get_sum_part_2(input):
     return sum(
