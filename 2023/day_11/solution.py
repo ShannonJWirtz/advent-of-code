be3632cb0ae0e8461abc7211f7162d063ab16c5d
Shannon Wirtz <shannon@mybuilder.com>
2023-12-11 19:32:45 +0000

    solution refactor

--- 2023/day_11/solution.py
+++ 2023/day_11/solution.py
@@ -1,40 +1,32 @@
 def get_inputs(filename):
-    with open(filename) as f: return [line.strip() for line in f.readlines()]
+    with open(filename) as f: 
+        return [line.strip() for line in f.readlines()]
 
-
-def part1(filename):
+def main(filename, expansion_factor=2):
     inputs = get_inputs(filename)
-    all_dot_rows = [i for i, line in enumerate(inputs) if all(char == '.' for char in line)]
-    all_dot_cols = [j for j in range(len(inputs[0])) if all(inputs[i][j] == '.' for i in range(len(inputs)))]
+    all_dot_rows = [i for i, line in enumerate(inputs) 
+                    if all(char == '.' for char in line)]
+    all_dot_cols = [j for j in range(len(inputs[0])) 
+                    if all(inputs[i][j] == '.' for i in range(len(inputs)))]
     galaxies = []
     for i, line in enumerate(inputs):
         for j, char in enumerate(line):
             if char == '#':
-                galaxies.append((i + sum(1 for n in all_dot_rows if n < i),
-                                 j + sum(1 for m in all_dot_cols if m < j)))
+                galaxies.append((i + sum(expansion_factor - 1 
+                                         for n in all_dot_rows if n < i),
+                                 j + sum(expansion_factor - 1 
+                                         for m in all_dot_cols if m < j)))
     lengths_total = 0
     for i, galaxy1 in enumerate(galaxies[:-1]):
         for galaxy2 in galaxies[i+1:]:
-            lengths_total += abs(galaxy1[0] - galaxy2[0]) + abs(galaxy1[1] - galaxy2[1])
+            lengths_total += (abs(galaxy1[0] - galaxy2[0]) + 
+                              abs(galaxy1[1] - galaxy2[1]))
     
     return lengths_total
 
-def part2(filename, expansion_factor):
-    inputs = get_inputs(filename)
-    all_dot_rows = [i for i, line in enumerate(inputs) if all(char == '.' for char in line)]
-    all_dot_cols = [j for j in range(len(inputs[0])) if all(inputs[i][j] == '.' for i in range(len(inputs)))]
-    galaxies = []
-    for i, line in enumerate(inputs):
-        for j, char in enumerate(line):
-            if char == '#':
-                galaxies.append((i + sum(expansion_factor - 1 for n in all_dot_rows if n < i),
-                                 j + sum(expansion_factor - 1 for m in all_dot_cols if m < j)))
-    lengths_total = 0
-    for i, galaxy1 in enumerate(galaxies[:-1]):
-        for galaxy2 in galaxies[i+1:]:
-            lengths_total += abs(galaxy1[0] - galaxy2[0]) + abs(galaxy1[1] - galaxy2[1])
-    
-    return lengths_total
+def part1(filename): return main(filename)
+
+def part2(filename, expansion_factor): return main(filename, expansion_factor)
 
 if __name__ == '__main__':
     print(f"Part 1: {part1('input.txt')}")
